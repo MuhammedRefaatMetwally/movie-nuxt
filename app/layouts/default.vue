5b37d46d15900c3762e13d2e01933b6c9232e486
Muhammed <muhammedrefaat2020@gmail.com>
2026-01-15 12:54:09 +0200

    first commit and setup of nuxt app and veutify

---
+++ app/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar extended>
+        <v-icon>mdi-house</v-icon>
+      <v-app-bar-title>Application</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+      <v-input></v-input>
+      
+     
+    </v-app-bar>
+
+    <v-main>
+      <slot/>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+
+</script>
